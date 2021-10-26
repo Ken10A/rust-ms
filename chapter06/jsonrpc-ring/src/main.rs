a6d9a4e4e64022b4b7049eca919ec9232497f2d5
KENN <cent.rngttwt@gmail.com>
2021-10-26 23:54:33 +0900

    [chapter06] add: jsonrpc implement

--- chapter06/jsonrpc-ring/src/main.rs
+++ chapter06/jsonrpc-ring/src/main.rs
@@ -53,6 +53,64 @@ fn to_internal<E: fmt::Display>(err: E) -> ServerError {
     ServerError::internal_error()
 }
 
-fn main() {
-    println!("Hello, world!");
+fn spawn_worker() -> Result<Sender<Action>, Error> {
+    let (tx, rx) = channel();
+    let next: SocketAddr = env::var("NEXT")?.parse()?;
+    thread::spawn(move || {
+        let remote = Remote::new(next);
+        let mut in_roll_call = false;
+        for action in rx.iter() {
+            match action {
+                Action::StartRollCall => {
+                    if !in_roll_call {
+                        if remote.start_roll_call().is_ok() {
+                            debug!("ON");
+                            in_roll_call = true;
+                        }
+                    } else {
+                        if remote.mark_itself().is_ok() {
+                            debug!("OFF");
+                            in_roll_call = false;
+                        }
+                    }
+                }
+                Action::MarkItself => {
+                    if in_roll_call {
+                        if remote.mark_itself().is_ok() {
+                            debug!("OFF");
+                            in_roll_call = false;
+                        }
+                    } else {
+                        debug!("SKIP");
+                    }
+                }
+            }
+        }
+    });
+    Ok(tx)
+}
+
+fn main() -> Result<(), Error> {
+    env_logger::init();
+    let tx = spawn_worker()?;
+    let addr: SocketAddr = env::var("ADDRESS")?.parse()?;
+    let mut io = IoHandler::default();
+    let sender = Mutex::new(tx.clone());
+    io.add_method(START_ROLL_CALL, move |_| {
+        trace!("START_ROLL_CALL");
+        let tx = sender.lock().map_err(to_internal)?;
+        tx.send(Action::StartRollCall)
+            .map_err(to_internal)
+            .map(|_| Value::Bool(true))
+    });
+
+    io.add_method(MARK_ITSELTF, move |_| {
+        trace!("MARK_ITSELF");
+        let tx = sender.lock().map_err(to_internal)?;
+        tx.send(Action::MarkItself)
+            .map_err(to_internal)
+            .map(|_| Value::Bool(true))
+    });
+    let server = ServerBuilder::new(io).start_http(&addr);
+    Ok(server.wait())
 }
