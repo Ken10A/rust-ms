76776efd6a037a90caf2f4313826abf453dd6aac
KENN <cent.rngttwt@gmail.com>
2021-10-27 23:59:28 +0900

    [chapter06] fix: make a separate sender to avoid move failure

--- chapter06/jsonrpc-ring/src/main.rs
+++ chapter06/jsonrpc-ring/src/main.rs
@@ -13,7 +13,7 @@ use std::sync::Mutex;
 use std::thread;
 
 const START_ROLL_CALL: &str = "start_roll_call";
-const MARK_ITSELTF: &str = "mark_itself";
+const MARK_ITSELF: &str = "mark_itself";
 
 enum Action {
     StartRollCall,
@@ -35,7 +35,7 @@ impl Remote {
         self.call_method(START_ROLL_CALL, &[])
     }
     fn mark_itself(&self) -> Result<bool, ClientError> {
-        self.call_method(MARK_ITSELTF, &[])
+        self.call_method(MARK_ITSELF, &[])
     }
     fn call_method<T>(&self, meth: &str, args: &[Value]) -> Result<T, ClientError>
     where
@@ -95,6 +95,7 @@ fn main() -> Result<(), Error> {
     let tx = spawn_worker()?;
     let addr: SocketAddr = env::var("ADDRESS")?.parse()?;
     let mut io = IoHandler::default();
+
     let sender = Mutex::new(tx.clone());
     io.add_method(START_ROLL_CALL, move |_| {
         trace!("START_ROLL_CALL");
@@ -104,13 +105,14 @@ fn main() -> Result<(), Error> {
             .map(|_| Value::Bool(true))
     });
 
-    io.add_method(MARK_ITSELTF, move |_| {
+    let sender = Mutex::new(tx.clone());
+    io.add_method(MARK_ITSELF, move |_| {
         trace!("MARK_ITSELF");
         let tx = sender.lock().map_err(to_internal)?;
         tx.send(Action::MarkItself)
             .map_err(to_internal)
             .map(|_| Value::Bool(true))
     });
-    let server = ServerBuilder::new(io).start_http(&addr);
+    let server = ServerBuilder::new(io).start_http(&addr)?;
     Ok(server.wait())
 }
